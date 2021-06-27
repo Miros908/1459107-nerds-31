625375599b5a7777ae3a45918eb7fde11966b04b
Miros908 <84718361+Miros908@users.noreply.github.com>
2021-06-27 19:09:40 +0300

    исправил замечания

--- script/popup.js
+++ script/popup.js
@@ -1,5 +1,19 @@
 let buttonwrite = document.querySelector(".button-write-us");
 let popup = document.querySelector(".popup");
 let buttonclose = document.querySelector(".button-close");
-buttonwrite.addEventListener("click", function() { popup.classList.remove("visually-hidden") });
-buttonclose.addEventListener("click", function() { popup.classList.add("visually-hidden") });
+
+
+buttonwrite.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("visually-hidden");
+    popup.classList.add("bounce");
+
+});
+
+
+
+
+buttonclose.addEventListener("click", function() {
+    popup.classList.add("visually-hidden");
+    popup.classList.remove("bounce")
+});
